1934755526dae7ee4a0c4abb7e663b653a6e0826
BluesClue27 <93749611+BluesClue27@users.noreply.github.com>
2024-09-26 12:54:26 +0800

    updated readme

--- settings.py
+++ settings.py
@@ -3,9 +3,9 @@ SERIAL_PORT = "/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Cont
 BAUD_RATE = 115200
 
 # API DETAILS
-# API_IP = '192.168.21.49'  # IP address of tim algo api
-API_IP = '192.168.21.74'
-# API_IP ='192.168.21.68' # IP address of img-recg api
+# API_IP = '192.168.21.49'  # IP address of Tim laptop
+API_IP = '192.168.21.74' # IP address of Alex laptop
+# API_IP ='192.168.21.68' # IP address of Ryan laptop
 API_PORT = 8000
 
 
